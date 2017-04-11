5645e2592392a795238be56c5e29cd5fe44beff5
nikolaTsenov <hyperniki@abv.bg>
2017-04-11 12:24:38 +0300

    assets/styleNav.css added, index.php added. Design not yet finished. Current design is not responsive.

--- README.md
+++
@@ -1,1 +0,0 @@
-"# Nikola-and-Stiliyan---Final-Project" 
---
+++ index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="elBag"/>
+	<meta http-equiv="author" name="Nikola Tsenov, Stiliyan Blagoev" />
+	<title>elBag</title>
+	<link rel="stylesheet" href="./assets/css/reset.css" type="text/css" />
+	<link rel="stylesheet" href="./assets/font-awesome-4.7.0/css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="./assets/css/styleNav.css" type="text/css" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+</head>
+<body>
+	<div id="wrapper">
+		<header id="header">
+			<nav id="userNav">
+				<h1 id="siteHeading"><a href="">elBag</a></h1>
+				<form id="searchingForm">
+  					<input type="text" name="search" placeholder="Search..">
+  					<input type="submit" name="submitSearch" value="Търси" />
+				</form>
+				<?php 
+				if (isset($_SESSION['username'])) {
+				?>
+				<ul id="personalNav">
+					<li><a href="#">Моят акаунт</a></li>
+					<li><a href="#"><i class="fa fa-heart" aria-hidden="true"></i> Фаворити</a></li>
+					<li><a href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Количка</a></li>
+				</ul>
+				<?php  
+				} else {
+				?>
+				<ul id="profileAccess">
+					<li><a href="#">Регистрация</a></li>
+					<li><a href="#">Влез в профила си</a></li>
+				</ul>
+				<?php 
+				}
+				?>
+			</nav>
+			<nav id="productNav">
+				<ul id="categories">
+					<li><a href="#">Телефони, Таблети &amp; Смарт технологии</a></li>
+					<li><a href="#">Лаптопи, IT продукти &amp; Офис</a></li>
+					<li><a href="#">ТВ, Електроника &amp; Фото</a></li>
+					<li><a href="#">Големи електроуреди</a></li>
+					<li><a href="#">Малки електроуреди</a></li>
+				</ul>
+			</nav>
+		</header>
+		<aside id="leftAside">
+			<h3 id="telCat">
+				Категории - Телефони, Таблети &amp; Смарт технологии
+			</h3>
+			<ul id="telList">
+				<li><a href="">Мобилни телефони</a></li>
+				<li><a href="">Смарт часовници</a></li>
+				<li><a href="">Таблети</a></li>
+				<li><a href="">Външни батерии</a></li>
+				<li><a href="">Аксесоари</a></li>
+				<li><a href="">Smart Home &amp; VR очила</a></li>
+			</ul>
+		</aside>
+	</div>
+</body>
+</html>
+
