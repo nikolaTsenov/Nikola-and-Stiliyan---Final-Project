6779ffc450289eccb548a387b84abd47e935feca
stilki4a <stilki4a@abv.bg>
2017-04-29 22:41:09 +0300

    ajax

---
+++ assets/js/products.js
@@ -0,0 +1,45 @@
+var div = document.getElementById("articles");
+// var btn = document.getElementById("btn");
+//
+// btn.addEventListener("click",function () {
+
+    var req = new XMLHttpRequest();
+    req.open('GET','http://localhost/php/Nikola-and-Stiliyan---Final-Project/controller/productController.php')
+    req.onload = function () {
+
+        var ourData = JSON.parse(req.responseText);
+        renderHTML(ourData);
+
+    }
+
+    req.send();
+
+
+// });
+
+function renderHTML(data) {
+
+    var htmlString = "";
+
+
+    for (var i =0; i<data.length; i++){
+        htmlString +="<div class='products'><a href='#'>"
+            // + data[i].products_name + " "
+            // + data[i].name + " "
+            // + data[i].price + " "
+            + "<img src =' "+ data[i].picture + "'  alt='tv'>"
+            // + data[i].warranty + " "
+            // + data[i].quantity + " "
+           +"<h3>" + data[i].model + "</h3></a>";
+            // + data[i].cname + " "
+            // + data[i].mname + " "
+        htmlString += "<a href='' id='link-button-for-articles'>"+"виж тук"+"</a>";
+        htmlString += "</div>";
+    }
+
+    div.insertAdjacentHTML('beforeend',htmlString);
+
+}
+
+
+
--- view/index.php
+++ view/index.php
@@ -37,77 +37,7 @@
 		<!-- In the below section we will load all adds from the database, I will leave it empty for now -->
 		<section id="articles">
 
-                <h1 class="categories-header">Телевизори</h1>
-
-            <div class="products">
-                <a href="#">
-                <img src="http://www.technopolis.bg/medias/sys_master/hf7/h94/9753428197406.jpg" alt="tv">
-                <h3>Тук ще стои линка на обявата и леко описание Тук ще стои линка на обявата и леко описани</h3></a>
-                <a href="" id="link-button-for-articles">виж тук</a>
-            </div>
-            <div class="products">
-                <a href="#">
-                    <img src="http://www.technopolis.bg/medias/sys_master/hf7/h94/9753428197406.jpg" alt="tv">
-                    <h3>Тук ще стои линка на обявата и леко описание Тук ще стои линка на обявата и леко описани</h3></a>
-                <a href="" id="link-button-for-articles">виж тук</a>
-            </div>
-            <div class="products">
-                <a href="#">
-                    <img src="http://www.technopolis.bg/medias/sys_master/hf7/h94/9753428197406.jpg" alt="tv">
-                    <h3>Тук ще стои линка на обявата и леко описание</h3></a>
-              <div>
-                  <a href="" id="link-button-for-articles">виж тук</a>
-              </div>
-            </div>
-            <div class="products">
-                <a href="#">
-                    <img src="http://www.technopolis.bg/medias/sys_master/hf7/h94/9753428197406.jpg" alt="tv">
-                    <h3>Тук ще стои линка на обявата и леко описание Тук ще стои линка на обявата и леко описани</h3></a>
-                <a href="" id="link-button-for-articles">виж тук</a>
-            </div>
-            <div class="products">
-                <a href="#">
-                    <img src="http://www.technopolis.bg/medias/sys_master/hf7/h94/9753428197406.jpg" alt="tv">
-                    <h3>Тук ще стои линка на обявата и леко описание Тук ще стои линка на обявата и леко описани</h3></a>
-                <a href="" id="link-button-for-articles">виж тук</a>
-            </div>
-            <div class="products">
-                <a href="#">
-                    <img src="http://www.technopolis.bg/medias/sys_master/hf7/h94/9753428197406.jpg" alt="tv">
-                    <h3>Тук ще стои линка на обявата и леко описание Тук ще стои линка на обявата и леко описани</h3></a>
-                <a href="" id="link-button-for-articles">виж тук</a>
-            </div>
-            <div class="products">
-                <a href="#">
-                    <img src="http://www.technopolis.bg/medias/sys_master/hf7/h94/9753428197406.jpg" alt="tv">
-                    <h3>Тук ще стои линка на обявата и леко описание Тук ще стои линка на обявата и леко описани</h3></a>
-                <a href="" id="link-button-for-articles">виж тук</a>
-            </div>
-            <div class="products">
-                <a href="#">
-                    <img src="http://www.technopolis.bg/medias/sys_master/hf7/h94/9753428197406.jpg" alt="tv">
-                    <h3>Тук ще стои линка на обявата и леко описание Тук ще стои линка на обявата и леко описани</h3></a>
-                <a href="" id="link-button-for-articles">виж тук</a>
-            </div>
-            <div class="products">
-                <a href="#">
-                    <img src="http://www.technopolis.bg/medias/sys_master/hf7/h94/9753428197406.jpg" alt="tv">
-                    <h3>Тук ще стои линка на обявата и леко описание Тук ще стои линка на обявата и леко описани</h3></a>
-                <a href="" id="link-button-for-articles">виж тук</a>
-            </div>
-            <div class="products">
-                <a href="#">
-                    <img src="http://www.technopolis.bg/medias/sys_master/hf7/h94/9753428197406.jpg" alt="tv">
-                    <h3>Тук ще стои линка на обявата и леко описание Тук ще стои линка на обявата и леко описани</h3></a>
-                <a href="" id="link-button-for-articles">виж тук</a>
-            </div>
-            <div class="products">
-                <a href="#">
-                    <img src="http://www.technopolis.bg/medias/sys_master/hf7/h94/9753428197406.jpg" alt="tv">
-                    <h3>Тук ще стои линка на обявата и леко описание Тук ще стои линка на обявата и леко описани</h3></a>
-                <a href="" id="link-button-for-articles">виж тук</a>
-            </div>
-
+                <h1 class="categories-header">ТЕЛЕФОН</h1>
         </section>
 <?php
 include_once "footer.php";
