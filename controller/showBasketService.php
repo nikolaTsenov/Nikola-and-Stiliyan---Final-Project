43fded8e821c4ead51770af1526f4859201391e6
stilki4a <stilki4a@abv.bg>
2017-05-02 15:19:53 +0300

    s

--- controller/showBasketService.php
+++ controller/showBasketService.php
@@ -9,12 +9,16 @@ spl_autoload_register('my_autoloader');
 $productite = new BasketDAO();
 $arr = $productite->getProductInBasket($user->id);
 
-var_dump($arr);
+//var_dump($arr);
 echo "<br />";
 
 for ($index = 0; $index < count($arr); $index++) {
-	echo "<img src='" . $arr[$index]['picture'] . "' alt=''";
-	echo $arr[$index]['products_name'];
-    echo $arr[$index]['manufacturers'];
-	echo "<br /><br />";
+    echo "<div class='products'><a href='../view/product.php?id='>";
+    echo "<img src='" . $arr[$index]['picture'] . "' alt=''>";
+	echo "<p>".$arr[$index]['products_name']."</p>";
+    echo "<p>".$arr[$index]['manufacturers']."</p>";
+    echo "<p>".$arr[$index]['price'].".лв</p>";
+
+	echo "</div>";
 }
+
